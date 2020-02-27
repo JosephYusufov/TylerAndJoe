9d5ab6383fb6f21c011fc151b0e1c4ff36cc96d9
Tyler Huang <thuang00@stuy.edu>
2020-02-26 21:55:40 -0500

    push

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -2,19 +2,30 @@ from pymongo import MongoClient
 from pprint import pprint
 import datetime
 import json
-client = MongoClient()
-db = client.test
-restaurants = db.restaurants
+
+client = MongoClient("mongodb://admin:thuang@64.225.14.222/")
+db = client.test_db
+restaurants = db.restaurants 
 
 
-with open("./primer-dataset.json", 'r') as file:
-    result = restaurants.insert_many(file)
+
+#with open("./primer-dataset.json", 'r') as file:
+#    result = restaurants.insert_many(file)
         # pprint(data)
 
-# post = {"author": "Mike",
-#         "text": "My first blog post!",
-#         "tags": ["mongodb", "python", "pymongo"],
-#         "date": datetime.datetime.utcnow()}
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()
+}
 
-print(result)
+#restaurants.insert_one(post)
+
+def findByZip(zip): 
+        print(restaurants.find({"zip" : zip}))        
+        #print(command) 
+
+
+findByZip(1)     
+#print(result)
 # pprint(posts.find_one())
